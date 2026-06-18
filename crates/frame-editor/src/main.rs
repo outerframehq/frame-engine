24f643ab34a3e465b3bdc48d8858d4990c2d8622
OuterFrameInteractive <24503849+OuterFrameInteractive@users.noreply.github.com>
2026-06-18 23:13:18 +0100

    Link frame-editor to frame-engine, create World from editor

--- crates/frame-editor/src/main.rs
+++ crates/frame-editor/src/main.rs
@@ -1,3 +1,14 @@
+use frame_engine::world::ComponentStorage;
+use frame_engine::world::World;
+
 fn main() {
-    println!("Hello, world!");
+    let world = World {
+        positions: ComponentStorage::new(),
+        velocities: ComponentStorage::new(),
+    };
+
+    println!(
+        "Editor Started, Engine World created with {} entities",
+        world.positions.len()
+    );
 }
