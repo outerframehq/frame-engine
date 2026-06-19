aa5bc1a695854572610e67867843be5bb0204476
OuterFrameInteractive <24503849+OuterFrameInteractive@users.noreply.github.com>
2026-06-19 19:12:39 +0100

    Draw solid color to editor window via softbuffer (native Wayland)

--- crates/frame-editor/src/main.rs
+++ crates/frame-editor/src/main.rs
@@ -1,21 +1,30 @@
 use frame_engine::world::ComponentStorage;
 use frame_engine::world::World;
+use std::num::NonZeroU32;
+use std::rc::Rc;
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, EventLoop};
 use winit::window::{Window, WindowId};
 
 struct App {
-    window: Option<Window>,
+    window: Option<Rc<Window>>,
+    surface: Option<softbuffer::Surface<Rc<Window>, Rc<Window>>>,
     world: World,
 }
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         let attributes = Window::default_attributes().with_title("Frame Editor");
-        let window = event_loop.create_window(attributes).unwrap();
+        let window = Rc::new(event_loop.create_window(attributes).unwrap());
+
+        let context = softbuffer::Context::new(window.clone()).unwrap();
+        let surface = softbuffer::Surface::new(&context, window.clone()).unwrap();
+
         window.request_redraw();
+
         self.window = Some(window);
+        self.surface = Some(surface);
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
@@ -25,8 +34,24 @@ impl ApplicationHandler for App {
                 event_loop.exit();
             }
             WindowEvent::RedrawRequested => {
-                if let Some(window) = &self.window {
-                    window.request_redraw();
+                if let (Some(window), Some(surface)) = (&self.window, &mut self.surface) {
+                    let size = window.inner_size();
+
+                    if let (Some(width), Some(height)) =
+                        (NonZeroU32::new(size.width), NonZeroU32::new(size.height))
+                    {
+                        surface.resize(width, height).unwrap();
+
+                        let mut buffer = surface.buffer_mut().unwrap();
+
+                        let color: u32 = (30 << 16) | (30 << 8) | 40; // drak blue grey
+
+                        for pixel in buffer.iter_mut() {
+                            *pixel = color;
+                        }
+
+                        buffer.present().unwrap();
+                    }
                 }
             }
             _ => {}
@@ -46,6 +71,7 @@ fn main() {
 
     let mut app = App {
         window: None,
+        surface: None,
         world,
     };
 
